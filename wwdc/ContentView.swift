44b6a9bbe80358ca905cb8a529b5cb9e25b80fcd
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-05-23 23:06:56 +0900

    Initial Commit

---
+++ wwdc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wwdc
+//
+//  Created by apple developer academy on 2022/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wwdc/wwdcApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwdcApp.swift
+//  wwdc
+//
+//  Created by apple developer academy on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct wwdcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
